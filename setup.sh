13a0ef69da66e37d945e2e84ee21d7e04db8a174
judahtc <jaycea247@gmail.com>
2025-09-02 10:39:01 +0200

    change the way to download awscli

--- setup.sh
+++ setup.sh
@@ -22,7 +22,20 @@ echo " Installing Python and pip..."
 sudo apt install -y python3 python3-pip
 
 echo " Installing AWS CLI..."
-sudo apt install -y awscli
+# Download the AWS CLI v2 installer
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Install unzip if not already installed
+sudo apt update && sudo apt install -y unzip
+
+# Unzip the installer
+unzip awscliv2.zip
+
+# Run the installer
+sudo ./aws/install
+
+# Verify installation
+aws --version
 
 echo " Versions:"
 docker --version
